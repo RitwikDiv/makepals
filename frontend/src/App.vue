9d9a0ae59147b5ad3d2e50e91548012f9e2cc1d6
ritwikdiv <ritwik.edu@gmail.com>
2021-02-21 17:52:28 -0500

    first commit - basic frontend commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="d-flex flex-row">
+    <Navbar />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
+#app {
+  font-family: "Poppins", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  border-top-style: solid;
+  border-width: 6px;
+  border-color: #4f359b;
+}
+</style>
