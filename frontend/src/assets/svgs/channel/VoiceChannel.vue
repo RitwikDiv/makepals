820947782cbd7ebb4ce3558aa88658aa8f6d7042
ritwikdiv <ritwik.edu@gmail.com>
2021-02-25 15:24:15 -0500

    values from data are taken to populate the cards

---
+++ frontend/src/assets/svgs/channel/VoiceChannel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card-header">
+    <strong>Voice Channel </strong>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="width"
+      :height="height"
+      :fill="color"
+      class="bi bi-mic-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z" />
+      <path
+        fill-rule="evenodd"
+        d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VoiceChannel",
+  props: {
+    color: {
+      type: String,
+      default: "#28a745",
+      required: false,
+    },
+    width: {
+      type: Number,
+      default: 16,
+      required: false,
+    },
+    height: {
+      type: Number,
+      default: 16,
+      required: false,
+    },
+  },
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -23,106 +23,22 @@
       <hr />
       <br />
       <div class="card-deck card-columns">
-        <div class="card">
-          <div class="card-header">
-            <strong>Voice Channel </strong>
-            <VoiceChannelLogo />
-          </div>
+        <div class="card" v-for="room in rooms" :key="room.id">
+          <div v-if="room.type == 'text'"><TextChannel /></div>
+          <div v-if="room.type == 'voice'"><VoiceChannel /></div>
           <div class="card-body">
-            <h5 class="card-title">WandaVision Discussion</h5>
-            <p class="card-meta text-muted">
-              <strong>Entertainment </strong>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="maroon"
-                class="bi bi-camera-reels-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M6 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path d="M9 6a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-                <path
-                  d="M9 6h.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 7.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 16H2a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h7z"
-                />
-              </svg>
-            </p>
-            <p class="card-text">
-              Meet some wonderful marvel nerds and theorize about the latest
-              episode of WandaVision!
-            </p>
-            <a href="" class="btn btn-outline-dark btn-sm"
-              ><strong>Join</strong></a
-            >
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <strong>Text Channel </strong>
-            <TextChannelLogo />
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Among Us Gaming Party</h5>
-            <p class="card-meta text-muted">
-              <strong>Gaming </strong>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="green"
-                class="bi bi-joystick"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2z"
-                />
-                <path
-                  d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23z"
-                />
-              </svg>
-            </p>
-            <p class="card-text">
-              Tired of playing among us alone with random people on the
-              internet? Join this chat and meet new people while playing the
-              game!
-            </p>
+            <h5 class="card-title">{{ room.title }}</h5>
+            <div v-if="room.category == 'gaming'"><GamingLogo /></div>
+            <div v-if="room.category == 'entertainment'">
+              <EntertainmentLogo />
+            </div>
+            <div v-if="room.category == 'reading'"><ReadingLogo /></div>
+            <p class="card-text">{{ room.desc }}</p>
             <a
-              href="https://discord.com/channels/812457952052903937/812524975356903495"
-              class="btn btn-outline-dark btn-sm"
+              :href="room.channelLink"
               target="_blank"
               rel="noopener noreferrer"
-              ><strong>Join</strong></a
-            >
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-header">
-            <strong>Text Channel </strong>
-            <TextChannelLogo />
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Obama's Promised Land</h5>
-            <p class="card-meta text-muted">
-              <strong>Reading </strong>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="orange"
-                class="bi bi-bookmark-star-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5zM8.16 4.1a.178.178 0 0 0-.32 0l-.634 1.285a.178.178 0 0 1-.134.098l-1.42.206a.178.178 0 0 0-.098.303L6.58 6.993c.042.041.061.1.051.158L6.39 8.565a.178.178 0 0 0 .258.187l1.27-.668a.178.178 0 0 1 .165 0l1.27.668a.178.178 0 0 0 .257-.187L9.368 7.15a.178.178 0 0 1 .05-.158l1.028-1.001a.178.178 0 0 0-.098-.303l-1.42-.206a.178.178 0 0 1-.134-.098L8.16 4.1z"
-                />
-              </svg>
-            </p>
-            <p class="card-text">
-              Join the chat and talk about Barack Obama's views and observations
-              conveyed in the book Promised land!
-            </p>
-            <a href="" class="btn btn-outline-dark btn-sm"
+              class="btn btn-outline-dark btn-sm"
               ><strong>Join</strong></a
             >
           </div>
@@ -133,18 +49,54 @@
 </template>
 
 <script>
-import TextChannelLogo from "../assets/svgs/TextChannelLogo.vue";
-import VoiceChannelLogo from "../assets/svgs/VoiceChannelLogo.vue";
+import TextChannel from "../assets/svgs/channel/TextChannel.vue";
+import VoiceChannel from "../assets/svgs/channel/VoiceChannel.vue";
+import ReadingLogo from "../assets/svgs/category/ReadingLogo.vue";
+import EntertainmentLogo from "../assets/svgs/category/EntertainmentLogo.vue";
+import GamingLogo from "../assets/svgs/category/GamingLogo.vue";
 
 export default {
   name: "Home",
   components: {
-    TextChannelLogo,
-    VoiceChannelLogo,
+    TextChannel,
+    VoiceChannel,
+    ReadingLogo,
+    EntertainmentLogo,
+    GamingLogo,
   },
   data() {
     return {
       images: { mainImage: require("@/assets/images/mainImage.png") },
+      rooms: [
+        {
+          id: 1,
+          title: "Among us Party",
+          desc:
+            "Tired of playing among us alone with random people on the internet? Join this chat and meet new people while playing the game!",
+          type: "voice",
+          category: "gaming",
+          channelLink:
+            "https://discord.com/channels/812457952052903937/812524975356903495",
+        },
+        {
+          id: 2,
+          title: "Obama's Promised Land",
+          desc:
+            "Join the chat and talk about Barack Obama's views and observations conveyed in the book Promised land!",
+          type: "text",
+          category: "reading",
+          channelLink: "",
+        },
+        {
+          id: 3,
+          title: "WandaVision Discussion",
+          desc:
+            "Meet some wonderful marvel nerds and theorize about the latest episode of WandaVision!",
+          type: "voice",
+          category: "entertainment",
+          channelLink: "",
+        },
+      ],
     };
   },
 };
