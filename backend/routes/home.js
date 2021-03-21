00c87768a11a1d85adfdde816eaa421dc0b22944
ritwikdiv <ritwik.edu@gmail.com>
2021-03-21 10:41:26 -0400

    basic api setup

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Api calls
 router.get('/', (req, res) => {
-    res.render('index', { title: "MakePals API", message: "Welcome to MakePals API! Check out the project github page to see the endpoints." });
+    res.send({ title: "MakePals API", message: "Welcome to MakePals API! Check out the project github page to see the endpoints." });
 });
 
 module.exports = router;
