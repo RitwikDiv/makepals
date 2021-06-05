89fcea4baba2cbffb426e3e7c8d69013b8dddf82
ritwikdiv <ritwik.edu@gmail.com>
2021-06-05 00:38:14 -0400

    added tailwindcss styles

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from "./App.vue"
-import './assets/css/bootstrap.css'
-import router from './router'
-import "@/assets/styles/main.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@/assets/styles/main.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+	.use(router)
+	.mount('#app');
