2ca30779add851e5ee0e896e0c9291657d0a5a17
ritwikdiv <ritwik.edu@gmail.com>
2021-06-29 00:50:27 -0400

    removing unncessary content/conduct pages

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,33 +1,27 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from "../views/Home.vue"
-import Conduct from "../views/Conduct.vue"
-import Create from "../views/Create.vue"
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Create from '../views/Create.vue';
 
 const routes = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/conduct',
-    name: 'Conduct',
-    component: Conduct
-  },
-  {
-    path: '/create',
-    name: 'Create',
-    component: Create
-  }
-]
+	{
+		path: '/',
+		redirect: '/home',
+	},
+	{
+		path: '/home',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/create',
+		name: 'Create',
+		component: Create,
+	},
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+	history: createWebHashHistory(),
+	routes,
+});
 
-export default router
+export default router;
